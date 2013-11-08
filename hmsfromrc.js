1a2eeea08fc42aa00f47b2b80476f08bd70cb9d0
tschmit007 <thierry.schmit@gmail.com>
2013-11-08 15:00:05 +0100

    intial

---
+++ hmsfromrc.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('#ztp_ar_ae_date').datepicker({
+        dateFormat: "yy-mm-dd",
+        constrainInput: true,
+        autoSize: true,
+        showButtonPanel: true
+    });
+});
